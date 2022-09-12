82cac770fe7c405bbfab364056fbe620455c4af2
Davi Gusmão <101421659+Davign10@users.noreply.github.com>
2022-09-12 01:01:14 -0300

    Update candidatoControler.java

--- candidatoControler.java
+++ candidatoControler.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Locale;
 
 import com.opencsv.CSVWriter;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,15 +18,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
-public class candidatoControler {
+public class candidatoCOntroler {
 
     @FXML
     private Button botaoAvancar;
 
     @FXML
     private TextField cpfCandidato;
-
+    
     @FXML
     private DatePicker dataCandidato;
 
@@ -45,12 +47,84 @@ public class candidatoControler {
     void cadastrarDados(ActionEvent event) {
         String nome = nomeCandidato.getText();
         String cpf = cpfCandidato.getText();
+        String formatado = cpf.substring(0, 3) + "." +
+                   cpf.substring(3, 6) + "." +
+                   cpf.substring(6, 9) + "-" +
+                   cpf.substring(9, 11);
         LocalDate data = dataCandidato.getValue();
         final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.ENGLISH);
         String data2 = dtf.format(data);
         String email = emailCandidato.getText();
         String senha = senhaCandidato.getText();
         String telefone = telefoneCandidato.getText();
+        if(nomeCandidato.getText().equals("")){
+            System.out.println("O CAMPO NOME É OBRIGATÓRIO! FAVOR PREENCHER!");
+            return;
+        }
+        if(cpfCandidato.getText().equals("")){
+            System.out.println("O CAMPO CPF É OBRIGATÓRIO! FAVOR PREENCHER!");
+            return;
+        }
+        if (cpfCandidato.getText().equals("00000000000")){
+            System.out.println("INSIRA UM CPF VÁLIDO!");
+            return;
+        }
+        if (cpfCandidato.getText().equals("11111111111")){
+            System.out.println("INSIRA UM CPF VÁLIDO!");
+            return;
+        }
+        if (cpfCandidato.getText().equals("22222222222")){
+            System.out.println("INSIRA UM CPF VÁLIDO!");
+            return;
+        }
+        if (cpfCandidato.getText().equals("33333333333")){
+            System.out.println("INSIRA UM CPF VÁLIDO!");
+            return;
+        }
+        if (cpfCandidato.getText().equals("44444444444")){
+            System.out.println("INSIRA UM CPF VÁLIDO!");
+            return;
+        }
+        if (cpfCandidato.getText().equals("55555555555")){
+            System.out.println("INSIRA UM CPF VÁLIDO!");
+            return;
+        }
+        if (cpfCandidato.getText().equals("66666666666")){
+            System.out.println("INSIRA UM CPF VÁLIDO!");
+            return;
+        }
+        if (cpfCandidato.getText().equals("77777777777")){
+            System.out.println("INSIRA UM CPF VÁLIDO!");
+            return;
+        }
+        if (cpfCandidato.getText().equals("88888888888")){
+            System.out.println("INSIRA UM CPF VÁLIDO!");
+            return;
+        }
+        if (cpfCandidato.getText().equals("99999999999")){
+            System.out.println("INSIRA UM CPF VÁLIDO!");
+            return;
+        }
+        //if (cpfCandidato.length() != 11){    
+        //}
+        
+        if(dataCandidato.getValue().equals("")){
+            System.out.println("O CAMPO DATA DE NASCIMENTO É OBRIGATÓRIO! FAVOR PREENCHER!");
+            return;
+        }
+        if(emailCandidato.getText().equals("")){
+            System.out.println("O CAMPO EMAIL É OBRIGATÓRIO! FAVOR PREENCHER!");
+            return;
+        }
+        if(senhaCandidato.getText().equals("")){
+            System.out.println("O CAMPO SENHA É OBRIGATÓRIO! FAVOR PREENCHER!");
+            return;
+        }
+        if(telefoneCandidato.getText().equals("")){
+            System.out.println("O CAMPO TELEFONE É OBRIGATÓRIO! FAVOR PREENCHER!");
+            return;
+        }
+        
 
         try {
             String caminho = "dados_candidato.csv";
@@ -73,5 +147,4 @@ public class candidatoControler {
             e.printStackTrace();
         }
     }
-
 }

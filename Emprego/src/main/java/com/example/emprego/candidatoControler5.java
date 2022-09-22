e22f66cc44c530f079f6403b828ad521c115bd76
larissasouz <lare-cpv@hotmail.com>
2022-09-21 22:42:56 -0300

    Apagando comentários e  Criando o controller5

---
+++ Emprego/src/main/java/com/example/emprego/candidatoControler5.java
@@ -0,0 +1,62 @@
+package com.example.emprego;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class candidatoControler5 {
+
+    @FXML
+    private Button botaoFinalizar;
+
+    @FXML
+    private TextField campoBairro;
+
+    @FXML
+    private ChoiceBox<?> campoCargo2;
+
+    @FXML
+    private TextField campoCep;
+
+    @FXML
+    private TextField campoCidade;
+
+    @FXML
+    private TextField campoComplemento;
+
+    @FXML
+    private TextField campoDescricao;
+
+    @FXML
+    private TextField campoEndereco;
+
+    @FXML
+    private TextField campoEstado;
+
+    @FXML
+    private TextField campoExperiencia;
+
+    @FXML
+    private CheckBox campoNao;
+
+    @FXML
+    private TextField campoNumero;
+
+    @FXML
+    private TextField campoPeriodo;
+
+    @FXML
+    private TextField campoSalario;
+
+    @FXML
+    private CheckBox campoSim;
+
+    @FXML
+    void finalizarVaga(ActionEvent event) {
+
+    }
+
+}

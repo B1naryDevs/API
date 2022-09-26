923d47a621db80e47905eb2efea5774d8d02f7a2
Davi Gusmão <101421659+Davign10@users.noreply.github.com>
2022-09-26 01:24:08 -0300

    Inserindo campos obrigatórios e validação

--- Emprego/src/main/java/com/example/emprego/candidatoControler2.java
+++ Emprego/src/main/java/com/example/emprego/candidatoControler2.java
@@ -2,10 +2,11 @@ package com.example.emprego;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
-
+import java.util.regex.Pattern;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -49,24 +50,44 @@ public class candidatoControler2 {
     void AvancarTela(ActionEvent event) throws FileNotFoundException {
         String empresa = campoEmpresa.getText();
         String cargo = campoCargo.getText();
+        String cargos = campoCargos.getText();
         LocalDate inicioinicial = campoInicio.getValue();
         LocalDate terminoinicial = campoTermino.getValue();
         String inicio = String.valueOf(inicioinicial);
         String termino = String.valueOf(terminoinicial);
         String descricao = campoDescricao.getText();
-        String cargos = campoCargos.getText();
         String pretensao = campoPretensao.getText();
 
-        Candidato candidato = new Candidato();
-        candidato.setExpEmpresa(empresa);
-        candidato.setCargo(cargo);
-        candidato.setExpInicio(inicio);
-        candidato.setExpTermino(termino);
-        candidato.setDescricao(descricao);
-        candidato.setCargoInteresse(cargos);
-        candidato.setPretSalarial(pretensao);
+        if (cargos.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("PREENCHA O CARGO DE INTERESSE!");
+            alert.showAndWait();
+        } else if (pretensao.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("PREENCHA A PRETENSÃO SALARIAL!");
+            alert.showAndWait();
+        } else {
+            // Validacao pretensão salarial, mínimo 3 digitos
+            String PRETENSAO_REGEX = "\\d{3,}";
+            Pattern PRETENSAO_PATTERN = Pattern.compile(PRETENSAO_REGEX);
+            if (PRETENSAO_PATTERN.matcher(pretensao).matches()) {
+                Candidato candidato = new Candidato();
+                candidato.setExpEmpresa(empresa);
+                candidato.setCargo(cargo);
+                candidato.setExpInicio(inicio);
+                candidato.setExpTermino(termino);
+                candidato.setDescricao(descricao);
+                candidato.setCargoInteresse(cargo);
+                candidato.setPretSalarial(pretensao);
 
-        HelloApplication.ChangeScene("candidato3");
+                HelloApplication.ChangeScene("candidato3");
+
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setHeaderText("FORMATO DE PRETENSÃO SALARIAL INVÁLIDO!");
+                alert.showAndWait();
+            }
+        }
     }
 
     @FXML

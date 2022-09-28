dabab3b105e3084a42470926e4fde00784ff5fdd
Wallace Honorato da Silva <101594950+WallaceHS20@users.noreply.github.com>
2022-09-27 23:34:34 -0300

    Add files via upload

--- Banco de Dados/banco.sql
+++ Banco de Dados/banco.sql
@@ -9,7 +9,7 @@ use api;
 # TABELAS SÓLIDAS ---------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------
 
-CREATE TABLE candidatos(
+CREATE TABLE candidato(
 nome_candidato varchar(30) not null,
 cpf bigint (11) not null,
 data_nasc datetime not null,
@@ -30,6 +30,7 @@ primary key (nome_rh)
 ); 
 
 CREATE TABLE vaga(
+cargo_vaga varchar(30) not null,
 id_vaga int(5) not null auto_increment,
 periodo varchar (20) not null,
 experiencia enum('Sim', 'Não') not null,
@@ -42,13 +43,13 @@ status_vaga enum('Aberta', 'Encerrada') not null,
 primary key (id_vaga)
 );
 
+drop table vaga;
+
 CREATE TABLE cargo(
 nome_cargo varchar(30) not null,
 descricao_cargo varchar(100),
 primary key (nome_cargo)
 );
-
-drop table cargo;
 
 CREATE TABLE formacao(
 id_formacao int(5) auto_increment not null,
@@ -83,23 +84,47 @@ primary key (id_comp)
 
 CREATE TABLE candidatura(
 id_candidatura int(5) auto_increment,
+cpf_candidatura bigint (11) not null,
+cod_vaga int(5) not null,
 data_cand datetime not null,
 status_cand enum('Em andamento', 'Desclassificado'),
 primary key (id_candidatura)
 );
 
+DROP TABLE candidatura;
+
 # CRIAÇÃO DE CHAVES ESTRANGEIRAS (FK) ----------------------------------------------------
 ------------------------------------------------------------------------------------------
 
-# CHAVE ESTRANGEIRA FK - CARGO || PARA VAGA 
-ALTER TABLE vaga
-ADD FOREIGN KEY (cargo_vaga)
-REFERENCES cargo(nome_cargo);
- 
+# CHAVE ESTRANGEIRA FK - CARGO PARA VAGA 
+ALTER TABLE vaga ADD FOREIGN KEY (cargo_vaga) REFERENCES cargo (nome_cargo);
+
+#CHAVE ESTRANGEIRA FK - CPF DO CANDIDATO PARA CANDIDATURA
+ALTER TABLE candidatura ADD FOREIGN KEY (cpf_candidatura) REFERENCES candidato (cpf);
+
+#CHAVE ESTRANGEIRA FK - CODIGO DA VAGA 
+ALTER TABLE candidatura ADD FOREIGN KEY (cod_vaga) REFERENCES vaga (id_vaga);
 
 
+# ACRESCENTAMENTO DE INFORMAÇÕES TESTES ---------------------------------------
+------------------------------------------------------------------------------
 
+insert into cargo (nome_cargo, descricao_cargo) values
+('Analista de Sistemas Junior', 'Desenvolver atividade 1'),
+('Suporte Técnco', 'Desenvolver atividade 2'),
+('Gerente Financeiro', 'Desenvolver atividade 3'),
+('Recpcionista', 'Desenvolver atividade 4'),
+('Desenvolvedor', 'Desenvolver atividade 5'),
+('Analista de Sistemas Pleno', 'Desenvolver atividade 6'),
+('Motorista', 'Desenvolver atividade 7'),
+('Advogado', 'Desenvolver atividade 8');
 
+select * from cargo;
 
+insert into vaga (cargo_vaga, id_vaga, periodo, experiencia, salario, descricao_vaga,endereco_vaga, cidade_vaga, remoto, status_vaga) values 
+('Analista de Sistemas Junior', default, 'Integral', 'Sim', 1450.22, 'Fatec-Testes', 'Rua Ribeirão do Vidoca 238 - Altos da Vila Paiva', 'São José dos Campos', 'Sim', 'Aberta'),
+('Advogado', default, 'Integral', 'Sim', 8250.50, 'Fatec-Testes', 'Rua Ribeirão do Vidoca 238 - Altos da Vila Paiva', 'São José dos Campos', 'Não', 'Aberta'),
+('Motorista', default, 'Noturno', 'Sim', 1200.00, 'Fatec-Testes', 'Rua Ribeirão do Vidoca 231 - Altossss da Vila Paiva', 'São José dos Campos', 'Sim', 'Aberta'),
+('Recpcionista', default, 'Manhã', 'Sim', 1100.22, 'Fatec-Testes', 'Rua Ribeirão do Vidoca 270 - Altosxxx da Vila Paiva', 'São José dos Campos', 'Não', 'Encerrada');
 
-
+select * from vaga;

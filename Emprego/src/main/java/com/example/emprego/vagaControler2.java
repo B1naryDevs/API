01ea986b2f7ae1f2a1fde8f9199410a04f26cdb2
Davi Gusmão <101421659+Davign10@users.noreply.github.com>
2022-09-30 14:23:50 -0300

    Inserção do botão cadastrar e o campo do cargo

--- Emprego/src/main/java/com/example/emprego/vagaControler2.java
+++ Emprego/src/main/java/com/example/emprego/vagaControler2.java
@@ -3,11 +3,18 @@ package com.example.emprego;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class vagaControler2 {
 
     @FXML
     private Button botaoHome;
+
+    @FXML
+    private Button botaoCadastrar;
+
+    @FXML
+    private TextField campoCargo2;
 
     @FXML
     void VoltarInicio(ActionEvent event) {

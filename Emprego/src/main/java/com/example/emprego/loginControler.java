6b749c2aca6e5ca00caa5ebb1735462983ec1aaf
Davi Gusmão <101421659+Davign10@users.noreply.github.com>
2022-10-04 02:04:41 -0300

    Inserção dos botões sair e ocultar/mostrar senha.

--- Emprego/src/main/java/com/example/emprego/loginControler.java
+++ Emprego/src/main/java/com/example/emprego/loginControler.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class loginControler {
 
@@ -15,10 +18,19 @@ public class loginControler {
     private Button botaoEntrar;
 
     @FXML
+    private ToggleButton botaoSair;
+    
+    @FXML
     private TextField campoEmail;
 
     @FXML
     private PasswordField campoSenha;
+    
+    @FXML
+    private TextField campoSenha2;
+
+    @FXML
+    private ToggleButton tgbtMostrarSenha;
 
     @FXML
     void cadastrarLogin(ActionEvent event) {
@@ -29,5 +41,24 @@ public class loginControler {
     void entrarLogin(ActionEvent event) {
         HelloApplication.ChangeScene("menu");
     }
+    
+    @FXML
+    void SairTela(ActionEvent event) { HelloApplication.ChangeScene("candidato2"); }
 
+    //ocultar/mostrar senha
+    @FXML
+    void mostrarSenha(ActionEvent event){
+        if(tgbtMostrarSenha.isSelected()){
+            campoSenha2.setText(campoSenha.getText());
+            campoSenha2.setVisible(true);
+            campoSenha.setVisible(false);
+        }else{
+            campoSenha.setText(campoSenha2.getText());
+            campoSenha.setVisible(true);
+            campoSenha2.setVisible(false);
+        }
+    }
 }
+
+
+

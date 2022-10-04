494b5b55189f3bd4644b8c52b5c82129583891f3
Davi Gusmão <101421659+Davign10@users.noreply.github.com>
2022-10-04 14:58:17 -0300

    Inserção dos botões: Home, Vaga, Cargo e Sair.

--- Emprego/src/main/java/com/example/emprego/menuControler.java
+++ Emprego/src/main/java/com/example/emprego/menuControler.java
@@ -3,23 +3,44 @@ package com.example.emprego;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 
 public class menuControler {
 
     @FXML
-    private Button botaoCargo;
+    private Button botaoHome;
 
     @FXML
     private Button botaoVaga;
 
     @FXML
-    void AvancarCargo(ActionEvent event) { HelloApplication.ChangeScene("cargo");
-
-    }
+    private Button botaoCargo;
 
     @FXML
-    void AvancarVaga(ActionEvent event) {
-        HelloApplication.ChangeScene("vaga");
-    }
+    private Button botaoRH;
+
+    @FXML
+    private Button botaoRelatorio;
+
+    @FXML
+    private ToggleButton botaoSair;
+
+    @FXML
+    void AvancarHome(ActionEvent event) {HelloApplication.ChangeScene("menu");}
+
+    @FXML
+    void AvancarVaga(ActionEvent event) {HelloApplication.ChangeScene("vaga");}
+
+    @FXML
+    void AvancarCargo(ActionEvent event) { HelloApplication.ChangeScene("cargo");}
+
+    @FXML
+    void AvancarRH(ActionEvent event) {HelloApplication.ChangeScene("");}
+
+    @FXML
+    void AvancarRelatorio(ActionEvent event) {HelloApplication.ChangeScene("");}
+
+    @FXML
+    void SairTela(ActionEvent event) {HelloApplication.ChangeScene("login");}
 
 }

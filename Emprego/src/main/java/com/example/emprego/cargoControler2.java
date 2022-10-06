32290a8d25ef5520726d768dec859150326dfc98
Davi Gusmão <101421659+Davign10@users.noreply.github.com>
2022-10-06 01:05:18 -0300

    Criação da tela de confirmação do cargo

---
+++ Emprego/src/main/java/com/example/emprego/cargoControler2.java
@@ -0,0 +1,48 @@
+package com.example.emprego;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.stage.Stage;
+
+public class cargoControler2 {
+
+    @FXML
+    private Button botaoHome;
+
+    @FXML
+    private Button botaoVaga;
+
+    @FXML
+    private Button botaoCargo;
+
+    @FXML
+    private Button botaoRH;
+
+    @FXML
+    private Button botaoRelatorio;
+
+    @FXML
+    private ToggleButton botaoSair;
+
+    @FXML
+    void AvancarHome(ActionEvent event) {HelloApplication.ChangeScene("menu");}
+
+    @FXML
+    void AvancarVaga(ActionEvent event) {HelloApplication.ChangeScene("vaga");}
+
+    @FXML
+    void AvancarCargo(ActionEvent event) { HelloApplication.ChangeScene("cargo");}
+
+    @FXML
+    void AvancarRH(ActionEvent event) {HelloApplication.ChangeScene("");}
+
+    @FXML
+    void AvancarRelatorio(ActionEvent event) {HelloApplication.ChangeScene("");}
+
+    @FXML
+    void SairTela(ActionEvent event) {HelloApplication.ChangeScene("login");}
+
+}

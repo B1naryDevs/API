f6bbba830afa28f5f7394ea4221f1e42aef1f155
Davi Gusmão <101421659+Davign10@users.noreply.github.com>
2022-10-20 16:07:16 -0300

    Criação da tela do Cadastro de RH

---
+++ Emprego/src/main/java/com/example/emprego/rhControler2.java
@@ -0,0 +1,102 @@
+package com.example.emprego;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class rhControler2  {
+
+    // Nomeclatura dos botões e campos
+    
+    @FXML
+    private Button botaoHome;
+
+    @FXML
+    private Button botaoCargos;
+
+    @FXML
+    private Button botaoVagas;
+
+    @FXML
+    private Button botaoRH;
+
+    @FXML
+    private Button botaoRelatorios;
+
+    @FXML
+    private Button botaoCadastrarFuncionarios;
+
+    @FXML
+    private Button botaoVisualizarFuncionarios;
+
+    @FXML
+    private Button botaoSobre;
+
+    @FXML
+    private ToggleButton botaoSair;
+
+    @FXML
+    private TextField campoNome;
+
+    @FXML
+    private TextField campoCpf;
+
+    @FXML
+    private TextField campoEmail;
+
+    @FXML
+    private TextField campoSenha;
+
+    @FXML
+    private TextField campoTelefone;
+
+    @FXML
+    private TextField campoConfirmarSenha;
+
+
+    // Definição das ações dos botões
+
+    @FXML
+    void AvancarHome(ActionEvent event) {HelloApplication.ChangeScene("menu");}
+
+    @FXML
+    void AvancarVagas(ActionEvent event) {HelloApplication.ChangeScene("");}
+
+    @FXML
+    void AvancarCargos(ActionEvent event) { HelloApplication.ChangeScene("");}
+
+    @FXML
+    void AvancarRH(ActionEvent event) {HelloApplication.ChangeScene("rh");}
+
+    @FXML
+    void AvancarSobre(ActionEvent event) {HelloApplication.ChangeScene("");}
+
+    @FXML
+    void AvancarRelatorios(ActionEvent event) {HelloApplication.ChangeScene("");}
+
+    @FXML
+    void CadastrarFuncionario(ActionEvent event) {HelloApplication.ChangeScene("");}
+
+    @FXML
+    void VoltarTela(ActionEvent event) {HelloApplication.ChangeScene("rh");}
+
+    @FXML
+    void SairTela(ActionEvent event) {HelloApplication.ChangeScene("login");}
+
+}
+

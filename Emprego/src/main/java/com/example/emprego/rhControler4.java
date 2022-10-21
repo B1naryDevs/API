0ced104b8f635c76f5ff346bfb71e7cfd4d89f87
Davi Gusmão <101421659+Davign10@users.noreply.github.com>
2022-10-21 16:15:33 -0300

    Criação da Tabela designada aos funcionários

---
+++ Emprego/src/main/java/com/example/emprego/rhControler4.java
@@ -0,0 +1,80 @@
+package com.example.emprego;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleButton;
+import javafx.stage.Stage;
+
+public class rhControler4  {
+
+    // Nomeclatura dos botões
+
+    @FXML
+    private Button botaoHome;
+
+    @FXML
+    private Button botaoCargos;
+
+    @FXML
+    private Button botaoVagas;
+
+    @FXML
+    private Button botaoRH;
+
+    @FXML
+    private Button botaoRelatorios;
+
+    @FXML
+    private Button botaoSobre;
+
+    @FXML
+    private ToggleButton botaoSair;
+
+    @FXML
+    private Button botaoSelecionar;
+
+
+    // Definindo os itens da tabela
+
+    @FXML
+    private TableView tabelaFuncionarios;
+
+    @FXML
+    private TableColumn colunaNome;
+
+    @FXML
+    private TableColumn colunaEmail;
+
+    @FXML
+    private TableColumn colunaTelefone;
+
+
+
+    // Definição das ações dos botões
+
+    @FXML
+    void AvancarHome(ActionEvent event) {HelloApplication.ChangeScene("menu");}
+
+    @FXML
+    void AvancarVagas(ActionEvent event) {HelloApplication.ChangeScene("vaga");}
+
+    @FXML
+    void AvancarCargos(ActionEvent event) { HelloApplication.ChangeScene("cargo");}
+
+    @FXML
+    void AvancarRH(ActionEvent event) {HelloApplication.ChangeScene("rh");}
+
+    @FXML
+    void AvancarSobre(ActionEvent event) {HelloApplication.ChangeScene("");}
+
+    @FXML
+    void AvancarRelatorios(ActionEvent event) {HelloApplication.ChangeScene("");}
+
+    @FXML
+    void SairTela(ActionEvent event) {HelloApplication.ChangeScene("login");}
+}
+
